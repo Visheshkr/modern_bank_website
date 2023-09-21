78ebcad59621b1ae88ccaaee357ab756742e8ef7
Vishesh <ranavisheshsingh242@gmail.com>
2023-09-21 22:24:58 +0530

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Clients from "./Clients";
+import Billing from "./Billing";
+import Business from "./Business";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+import CardDeal from "./CardDeal";
+
+export{
+    Navbar,
+    Hero,
+    Billing,
+    Clients,
+    Business,
+    CTA,
+    Stats,
+    Testimonials,
+    Footer,
+    CardDeal
+};
